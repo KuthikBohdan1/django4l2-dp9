7bb77a4883ba806a4507306f6d1ccb2d8b8263ae
KuthikBohdan1 <bkuzik14@gmail.com>
2025-06-06 23:49:00 +0300

    2

--- app/models.py
+++ app/models.py
@@ -3,12 +3,17 @@ from django.db import models
 
 
 class LibraryBook(models.Model):
-    title = models.CharField()
-    author = models.CharField()
-    isbn = models.CharField()
+    title = models.CharField(max_length=256)
+    author = models.CharField(max_length=256)
+    isbn = models.CharField(max_length=256, unique=True)
 
-    availble = models.BooleanField()
+    availble = models.BooleanField(default=True)
 
+    def __str__(self):
+        return f"{self.title} - {self.author}"
+    
 
     class Meta:
-        
+        # verbose_name = "author"
+        # verbose_name_plural = "libraryBooks"
+        ordering = ["author"] #сортування
